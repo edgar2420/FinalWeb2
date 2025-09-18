fc3e18e254c199aba39215f826aafbb905371028
Edgar Rojas <90846268+edgar2420@users.noreply.github.com>
2025-09-18 09:09:15 -0400

    Update authService.js

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/auth"; // Cambia según tu backend
+const API_URL = "http://localhost:3000/auth";
 
 export const login = async (email, password) => {
   try {
